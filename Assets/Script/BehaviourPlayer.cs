c580acce6d23ae948093bba6054d10482bb2675f
chenlaug <ch.laughan@gmail.com>
2025-04-20 04:21:07 +0200

    Feat: add rotation camera to player

--- Assets/Script/BehaviourPlayer.cs
+++ Assets/Script/BehaviourPlayer.cs
@@ -6,5 +6,22 @@ public class BehaviourPlayer : MonoBehaviour
 
     private float _xRotation;
     private float _mouseSensibility = 100f;
+    private readonly float _limiteRotationX = 70.0f;
 
+    private void Update()
+    {
+        BehaviourPlayerLook();
+    }
+
+    private void BehaviourPlayerLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensibility * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensibility * Time.deltaTime;
+        
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -_limiteRotationX, _limiteRotationX);
+        
+        camera.transform.localRotation = Quaternion.Euler(_xRotation,0.0f,0.0f);
+        gameObject.transform.Rotate(Vector3.up * mouseX);
+    }
 }
