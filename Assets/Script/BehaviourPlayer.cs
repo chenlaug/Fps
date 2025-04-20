57aaa94f085f71358dd77d6b4e8c034c76806ec2
chenlaug <ch.laughan@gmail.com>
2025-04-20 04:27:12 +0200

    feat: add controller to player

--- Assets/Script/BehaviourPlayer.cs
+++ Assets/Script/BehaviourPlayer.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class BehaviourPlayer : MonoBehaviour
 {
     [SerializeField] private new Camera camera;
+    [SerializeField] private CharacterController characterController;
 
     private float _xRotation;
     private float _mouseSensibility = 100f;
     private readonly float _limiteRotationX = 70.0f;
+    private readonly float _moveSpeed = 10.0f;
 
     private void Update()
     {
         BehaviourPlayerLook();
+        BehaviourPlayerMove();
     }
 
     private void BehaviourPlayerLook()
@@ -23,5 +26,13 @@ public class BehaviourPlayer : MonoBehaviour
         
         camera.transform.localRotation = Quaternion.Euler(_xRotation,0.0f,0.0f);
         gameObject.transform.Rotate(Vector3.up * mouseX);
+    }
+    private void BehaviourPlayerMove()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = gameObject.transform.right * moveX + gameObject.transform.forward * moveZ;
+        characterController.Move(move * (_moveSpeed * Time.deltaTime));
     }
 }
