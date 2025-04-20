9c46fa9cc565eed5150e3190b704daa8fc741028
chenlaug <ch.laughan@gmail.com>
2025-04-20 04:09:58 +0200

    Feat: add Player and Enemy prefabs

---
+++ Assets/Script/BehaviourPlayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class BehaviourPlayer : MonoBehaviour
+{
+    [SerializeField] private new Camera camera;
+
+    private float _xRotation;
+    private float _mouseSensibility = 100f;
+
+}
