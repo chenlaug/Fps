d405994721cae28f9c1f9b8d979143ee46642243
chenlaug <ch.laughan@gmail.com>
2025-04-21 20:35:06 +0200

    Feat: add Instantiate bullet

--- Assets/Script/BehaviourWeapon.cs
+++ Assets/Script/BehaviourWeapon.cs
@@ -1,12 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class BehaviourWeapon : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject bullet;
+    [SerializeField] private GameObject bulletPrefab;
 
     [SerializeField] private GameObject cannonWeapon;
+
+    private void Update()
+    {
+        InstantiateBullet();
+    }
+
+    private void InstantiateBullet()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.transform.position = cannonWeapon.transform.position;
+        }
+    }
 }
